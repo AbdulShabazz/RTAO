a0bfa4c01c54b3e07e0e24fab4e28c27aed3e8b0
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-04-01 09:19:43 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 @echo off
+bin\RTAO.exe -width 1920 -height 1080 -model ".\models\cinema.obj" -model ".\models\quad.obj"
 bin\RTAO.exe -width 1920 -height 1080 -vsync 0 -scene ".\models\cinema.obj" -scene ".\models\quad.obj" 
